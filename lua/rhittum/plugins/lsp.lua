497d316a4c6a24817fc77cee14821859d791b81e
Rhittum <rhittum1802@gmail.com>
2025-05-10 18:55:05 +0000

    updated lsp diagnostics, added trouble for better diagnostics and errors.

--- lua/rhittum/plugins/lsp.lua
+++ lua/rhittum/plugins/lsp.lua
@@ -6,5 +6,14 @@ return {
     require("mason-lspconfig").setup({
 	    ensure_installed = { "lua_ls","pyright", "ts_ls", "jdtls", "rust_analyzer", "clangd",},
     })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+    vim.diagnostic.config({
+	    virtual_text = true,
+	    signs = true,
+	    update_in_insert = false,
+	    severity_sort = true,
+    })
+    vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { desc = "LSP Signature Help" })
     end
 }
